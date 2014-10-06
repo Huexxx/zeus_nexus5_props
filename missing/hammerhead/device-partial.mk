86fe008ee46d6048feef72e641c8cf9e5cc0fbe9
Huexxx <huexxx@gmail.com>
2014-10-06 22:20:30 +0200

    Remove gapps files

--- missing/hammerhead/device-partial.mk
+++ missing/hammerhead/device-partial.mk
@@ -35,13 +35,6 @@ PRODUCT_COPY_FILES := \
     vendor/missing/hammerhead/proprietary/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so:lge \
     vendor/missing/hammerhead/proprietary/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd:lge \
     vendor/missing/hammerhead/proprietary/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd:lge \
-    vendor/missing/hammerhead/proprietary/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin:lge \
-    vendor/missing/hammerhead/proprietary/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin:lge \
-    vendor/missing/hammerhead/proprietary/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin:lge \
-    vendor/missing/hammerhead/proprietary/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin:lge \
-    vendor/missing/hammerhead/proprietary/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin:lge \
-    vendor/missing/hammerhead/proprietary/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin:lge \
-    vendor/missing/hammerhead/proprietary/face.face.y0-y0-22-b-N.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin:lge \
     vendor/missing/hammerhead/proprietary/OmaDmclient.apk:system/app/OmaDmclient.apk:lge \
     vendor/missing/hammerhead/proprietary/shutdownlistener.apk:system/app/shutdownlistener.apk:lge \
     vendor/missing/hammerhead/proprietary/shutdownlistener.odex:system/app/shutdownlistener.odex:lge \
