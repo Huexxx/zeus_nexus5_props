ee8c93a414fe010e44b4e6209d22a518f9eecd33
Huexxx <huexxx@gmail.com>
2014-10-07 22:48:21 +0200

    Use deodex prebuilts from google site instead the KTU84Q image ones

--- lge/hammerhead/device-partial.mk
+++ lge/hammerhead/device-partial.mk
@@ -15,10 +15,8 @@
 # LGE blob(s) necessary for Hammerhead hardware
 PRODUCT_COPY_FILES := \
     vendor/lge/hammerhead/proprietary/qcrilmsgtunnel.apk:system/app/qcrilmsgtunnel.apk:lge \
-    vendor/lge/hammerhead/proprietary/qcrilmsgtunnel.odex:system/app/qcrilmsgtunnel.odex:lge \
     vendor/lge/hammerhead/proprietary/SprintHiddenMenu.apk:system/app/SprintHiddenMenu.apk:lge \
     vendor/lge/hammerhead/proprietary/UpdateSetting.apk:system/app/UpdateSetting.apk:lge \
-    vendor/lge/hammerhead/proprietary/UpdateSetting.odex:system/app/UpdateSetting.odex:lge \
     vendor/lge/hammerhead/proprietary/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb:lge \
     vendor/lge/hammerhead/proprietary/General_cal.acdb:system/etc/General_cal.acdb:lge \
     vendor/lge/hammerhead/proprietary/Global_cal.acdb:system/etc/Global_cal.acdb:lge \
